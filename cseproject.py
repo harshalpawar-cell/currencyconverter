7595f1928e83fbe642cabceef96dcae28e1e958e
harshalpawar-cell <harshal8478pawar@gmail.com>
2025-11-22 21:22:21 +0530

    cseproject.py

--- cseproject.py
+++ cseproject.py
@@ -13,37 +13,3 @@ m=int(input("Enter no. for currency to convert: "))
 amount=float(input("Enter amount in " + currency[n] + ": "))
 k=amount*rate[n][m]
 print(amount, currency[n], "is equal to", k, currency[m])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print("Available currencies:","\n","1: USD","\n","2: INR","\n","3: EUR")
-# h=int(input("Enter the no. of currency:"))
-# m=int(input("Enter the no. of currency:"))
-# p=int(input("Enter amount:"))
-# if h==1 and m==2:
-#     print(p,"USD is equal to",p*87,"INR")
-# elif h==1 and m==3:
-#     print(p,"USD is equal to",p*0.87,"EUR")
-# elif h==2 and m==3:
-#     print(p,"INR is equal to",p*0.0097,"EUR")
-# elif h==3 and m==1:
-#     print(p,"EUR is equal to",p*1.15,"USD")
-# elif h==3 and m==2:
-#     print(p,"EUR is equal to",p*102.97,"INR")
-# elif h==2 and m==1:
-#     print(p,"INR is equal to",p*0.011,"USD")
-# else: print("None")
